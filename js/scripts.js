e937f87cf33cae3307b9dc1d0f02c949b6e8f0ae
Hunter Carey <huntercarey902@gmail.com>
2019-10-14 14:01:44 -0500

    Set up the dark sky call, moved API key to a seperate file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-console.log('test');
+(function () {
+    getWeatherInfo();
+})();
+
+//Function to connect to Dark Sky API and get weather data
+function getWeatherInfo() {
+    //https://api.darksky.net/forecast/e23e5d8cad46646375f6c7d174b48a11/37.8267,-122.4233
+    //Base URL/APIKey/Latitude,Longitude
+    
+
+    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", {dataType: "jsonp" })
+   .done(function(data) {
+       console.log(data);
+   })
+   .fail(function(error){
+       console.log(error);
+   })
+   .always(function(){
+       console.log("Weather call complete!");
+   })
+}
+
+//Function to connect to the MapQuest Geocoding API and get geocoding data
+function geocode() {
+
+}
