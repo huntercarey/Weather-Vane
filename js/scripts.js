eed63c7228f61075429581322fbb92ccca029bc0
Hunter Carey <huntercarey902@gmail.com>
2019-10-17 14:51:40 -0500

    set up template to use data from API call and added the ability to remove cards with the #remove button

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,28 @@
         geocode(searchLocation);
         //Clear out the search bar
         $('#searchBar').val('');
-    })
+    });
+    //ENTERKEY
+    $('#searchBar').keydown(function(event) {
+        if (event.keyCode === 13) {
+            event.preventDefault(); //Stops the key from doing it's default behavior
+            const searchLocation = $('#searchBar').val();
+            // Call the geocode function and pass in the value
+            geocode(searchLocation);
+            // Clear out the search bar
+            $('#searchBar').val('');
+        }
+    });
+
+    $(document).on('click', 'button#remove', function () {
+        //Get the parent element of the button
+        let parentDiv = $(this).parent(); //THIS refers to the element that triggered the event handler (in this case the button that was clicked)
+        //Get the parent of the div containing the button
+        let weatherCardContainer = parentDiv.parent();
+        //remove the container and all of its contents
+        weatherCardContainer.remove();
+    });
+
 })();
 
 //Function to connect to Dark Sky API and get weather data
@@ -18,7 +39,35 @@ function getWeatherInfo(latitude, longitude, city, state) {
 
     $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude, {dataType: "jsonp" })
    .done(function(data) {
-       console.log(data);
+       //Get the html from the div with the ID of 'template'
+       let templateHTML = $('#template').html();
+
+       //We need to get the temperature from dark sky data
+       let temperature = data.currently.temperature;
+       let conditions = data.currently.summary;
+
+       let currentDayInfo = data.daily.data[0];
+       let highTemp = Math.round(currentDayInfo.temperatureHigh);
+       let lowTemp = Math.round(currentDayInfo.temperatureLow);
+       let precipChance = Math.round(currentDayInfo.precipProbability * 100);
+
+       //Replacing the string '@@city@@' with the city we pass into this function in the html
+       templateHTML = templateHTML.replace('@@city@@', city);
+       //replace the string '@@currentTemp@@' with the temperature we get back from the API call
+       templateHTML = templateHTML.replace('@@currentTemp@@', Math.round(temperature));
+       //Replace the string '@@cityState' with the city we pass into the function
+       templateHTML = templateHTML.replace('@@cityState@@', city + ' ' + state);
+       //replace @@conditions@@ with conditions from API
+       templateHTML = templateHTML.replace('@@conditions@@', conditions);
+       //replace @@highTemp@@ with high temp from API
+       templateHTML = templateHTML.replace('@@highTemp@@', highTemp);
+        //repeat
+       templateHTML = templateHTML.replace('@@lowTemp@@', lowTemp);
+        //repeat
+       templateHTML = templateHTML.replace('@@precipitation@@', precipChance);
+
+       //Add the configured templateHTML to our row in the card container
+       $('.row').append(templateHTML);
    })
    .fail(function(error){
        console.log(error);
